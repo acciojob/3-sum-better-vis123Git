bcd270bbcb98dd5e31f2f29300fbb641d73314cf
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-03 08:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function threeSum(arr, target) {
-// write your code here
+  arr.sort((a, b) => a - b);
+
+  let closestSum = arr[0] + arr[1] + arr[2];
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
